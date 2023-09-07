a51114478c0476ed5e73aa267f7e7056c19991eb
Dr3a0p <dreandreax@icloud.com>
2023-09-07 10:07:37 -0500

    adding to my java file

--- assets/script.js
+++ assets/script.js
@@ -1,190 +1,279 @@
-var wordBlank = document.querySelector(".word-blanks");
-var win = document.querySelector(".win");
-var lose = document.querySelector(".lose");
-var timerElement = document.querySelector(".timer-count");
-var startButton = document.querySelector(".start-button");
+var startBtn = document.getElementById("startBtn");
+var time = 75;
+var time_remaining = true;
+var time_start= false;
+var countdownTimer = document.getElementById("countdownTimer");
+var homeContainer =  document.getElementById("homeContainer");
+var quizContainer = document.getElementById("quizContainer");
+var questionHeading = document.getElementById("questionHeading");
+var answerChoiceA = document.getElementById("answerChoiceA");
+var answerChoiceB = document.getElementById("answerChoiceB");
+var answerChoiceC = document.getElementById("answerChoiceC");
+var answerChoiceD = document.getElementById("answerChoiceD");
+var correctAnswer = document.getElementById("correctAnswer");    
+var high_scores= [];
+var output="";
+// Set score = 0 at the start of the game 
+var score = 0;
+// question index
+let i = 0;
 
-var chosenWord = "";
-var numBlanks = 0;
-var winCounter = 0;
-var loseCounter = 0;
-var isWin = false;
-var timer;
-var timerCount;
+// QUESTIONS ARRAY:
 
-// Arrays used to create blanks and letters on screen
-var lettersInChosenWord = [];
-var blanksLetters = [];
+var questionsArray = [
+{
+question: "Question: What is the HTML tag under which you can write the JavaScript code?",
+imageSrc: "",
+answerChoice: ["A) <javascript>", "B) <scripted>", "C) <script>", "D) <js>"],
+correctAnswer: 2
+}, 
+{
+question: "Question: What are variables used for in JavaScript Programs?",
+imageSrc: "",
+answerChoice: ["A) Storing numbers, dates, or other values", "B) Varying randomly", "D) Causing high-school algebra flashbacks", "D) None of the above"],
+correctAnswer: 0
+},
+{
+question: "Question: Which method adds a new item to the end of an array and returns the new length?",
+imageSrc: "",
+answerChoice: ["A) shift()", "B) return() ", "C) push() ", "D) pop()"],
+correctAnswer: 2
+}, 
+{
+question: "Question: Which of the following can't be done with client-side JavaScript?",
+imageSrc: "",
+answerChoice: ["A) Sending a form's contents by email", "B) Validating a form", "C) Storing the form's contents to a database file on the server", "D) None of the above"],
+correctAnswer: 2
+},
+{
+question: "Question: Which of the following are capabilities of functions in JavaScript?",
+answerChoice: ["A) Return a value", "B) Accept parameters", "C) Accept parameters and Return a value", "D) All of the above"],
+correctAnswer: 1
+}];
 
-// Array of words the user will guess
-var words = ["variable","array", "modulus", "object", "function", "string", "boolean"];
+//COUNTDOWN TIMER FUNCTION: set countdown timer and interval. Set time-related valiables.
 
-// The init function is called when the page loads 
-function init() {
-  getWins();
-  getlosses();
-}
+//change the seconds variable every second.
+var countdownTimerInterval = setInterval(setCountdownTimer, 1000);
 
-// The startGame function is called when the start button is clicked
-function startGame() {
-  isWin = false;
-  timerCount = 10;
-  // Prevents start button from being clicked when round is in progress
-  startButton.disabled = true;
-  renderBlanks()
-  startTimer()
-}
-
-// The winGame function is called when the win condition is met
-function winGame() {
-  wordBlank.textContent = "YOU WON!!!🏆 ";
-  winCounter++
-  startButton.disabled = false;
-  setWins()
-}
-
-// The loseGame function is called when timer reaches 0
-function loseGame() {
-  wordBlank.textContent = "GAME OVER";
-  loseCounter++
-  startButton.disabled = false;
-  setLosses()
-}
-
-// The setTimer function starts and stops the timer and triggers winGame() and loseGame()
-function startTimer() {
-  // Sets timer
-  timer = setInterval(function() {
-    timerCount--;
-    timerElement.textContent = timerCount;
-    if (timerCount >= 0) {
-      // Tests if win condition is met
-      if (isWin && timerCount > 0) {
-        // Clears interval and stops timer
-        clearInterval(timer);
-        winGame();
-      }
+//function that changes the time var
+function setCountdownTimer() {
+    if (time_start)
+    time--;
+    if(time<= 0) {
+    end_quiz();
+    time = 0;    
+    // clearInterval(countdownTimerInterval);
+    //alert user and stop quiz
     }
-    // Tests if time has run out
-    if (timerCount === 0) {
-      // Clears interval
-      clearInterval(timer);
-      loseGame();
-    }
-  }, 1000);
+    document.getElementById("timer").innerHTML = time;
 }
 
-// Creates blanks on screen
-function renderBlanks() {
-  // Randomly picks word from words array
-  chosenWord = words[Math.floor(Math.random() * words.length)];
-  lettersInChosenWord = chosenWord.split("");
-  numBlanks = lettersInChosenWord.length;
-  blanksLetters = []
-  // Uses loop to push blanks to blankLetters array
-  for (var i = 0; i < numBlanks; i++) {
-    blanksLetters.push("_");
-  }
-  // Converts blankLetters array into a string and renders it on the screen
-  wordBlank.textContent = blanksLetters.join(" ")
-}
-
-// Updates win count on screen and sets win count to client storage
-function setWins() {
-  win.textContent = winCounter;
-  localStorage.setItem("winCount", winCounter);
-}
-
-// Updates lose count on screen and sets lose count to client storage
-function setLosses() {
-  lose.textContent = loseCounter;
-  localStorage.setItem("loseCount", loseCounter);
-}
-
-// These functions are used by init
-function getWins() {
-  // Get stored value from client storage, if it exists
-  var storedWins = localStorage.getItem("winCount");
-  // If stored value doesn't exist, set counter to 0
-  if (storedWins === null) {
-    winCounter = 0;
-  } else {
-    // If a value is retrieved from client storage set the winCounter to that value
-    winCounter = storedWins;
-  }
-  //Render win count to page
-  win.textContent = winCounter;
-}
-
-function getlosses() {
-  var storedLosses = localStorage.getItem("loseCount");
-  if (storedLosses === null) {
-    loseCounter = 0;
-  } else {
-    loseCounter = storedLosses;
-  }
-  lose.textContent = loseCounter;
-}
-
-function checkWin() {
-  // If the word equals the blankLetters array when converted to string, set isWin to true
-  if (chosenWord === blanksLetters.join("")) {
-    // This value is used in the timer function to test if win condition is met
-    isWin = true;
-  }
-}
-
-// Tests if guessed letter is in word and renders it to the screen.
-function checkLetters(letter) {
-  var letterInWord = false;
-  for (var i = 0; i < numBlanks; i++) {
-    if (chosenWord[i] === letter) {
-      letterInWord = true;
-    }
-  }
-  if (letterInWord) {
-    for (var j = 0; j < numBlanks; j++) {
-      if (chosenWord[j] === letter) {
-        blanksLetters[j] = letter;
-      }
-    }
-    wordBlank.textContent = blanksLetters.join(" ");
-  }
-}
-
-// Attach event listener to document to listen for key event
-document.addEventListener("keydown", function(event) {
-  // If the count is zero, exit function
-  if (timerCount === 0) {
-    return;
-  }
-  // Convert all keys to lower case
-  var key = event.key.toLowerCase();
-  var alphabetNumericCharacters = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
-  // Test if key pushed is letter
-  if (alphabetNumericCharacters.includes(key)) {
-    var letterGuessed = event.key;
-    checkLetters(letterGuessed)
-    checkWin();
-  }
+// When user clicks Start button, start the countdown timer and quiz questions. Add an event listener to each button.
+startBtn.addEventListener("click", function() {
+quizContainer.style.display = "block";
+information.style.display ="none";
+countdownTimer.style.display= "block";
+document.getElementById("score_keeper").style.display= "block";
+document.getElementById("score").innerHTML = score;
+setCountdownTimer();
+setQuizQuestions();
+time_start= true;
 });
 
-// Attach event listener to start button to call startGame function on click
-startButton.addEventListener("click", startGame);
+// QUESTIONS FUNCTION: display questions and multiple-choice answers
 
-// Calls init() so that it fires when page opened
-init();
+function setQuizQuestions() {
+    questionHeading.textContent = questionsArray[i].question;
+    answerChoiceA.textContent = questionsArray[i].answerChoice[0]; 
+    answerChoiceB.textContent = questionsArray[i].answerChoice[1]; 
+    answerChoiceC.textContent = questionsArray[i].answerChoice[2]; 
+    answerChoiceD.textContent = questionsArray[i].answerChoice[3]; 
+    };
 
-// Bonus: Add reset button
-var resetButton = document.querySelector(".reset-button");
+// When user answers a question: then user is presented with another question
 
-function resetGame() {
-  // Resets win and loss counts
-  winCounter = 0;
-  loseCounter = 0;
-  // Renders win and loss counts and sets them into client storage
-  setWins()
-  setLosses()
+// Store user answer choices. Clear elements and update score count.
+
+// Change to next question
+answerChoiceA.addEventListener('click', function(event) {
+    event.stopPropagation();
+    correctAnswer= questionsArray[i].correctAnswer;
+    console.log("correctAnswer " + correctAnswer);
+    // check answer
+    if (0 === correctAnswer) { 
+        // display message to user for 1  second stating if the answer is correct or incorrect
+        document.getElementById("AnswerResponse").innerHTML = "Correct! Nailed it!";
+        setTimeout(function() {
+        document.getElementById("AnswerResponse").innerHTML = "";
+            },
+            1000
+        );
+        // when user answers a question correctly, increase the score
+        score++;    
+        // display updated score progress
+        document.getElementById("score").innerHTML = score;
+    } else {
+        time_remaining -= 5;
+        // when user answers a question inccorrectly, subtract from the time
+        document.getElementById("AnswerResponse").innerHTML = "Incorrect! Better luck in the next one!";
+        setTimeout(function() {
+            document.getElementById("AnswerResponse").innerHTML = "";
+                },
+                1000
+            );
+    }
+    if (i >= questionsArray.length -1) {
+    end_quiz();
+    } else {
+        i++ 
+        setQuizQuestions();
+    };
+});
+
+answerChoiceB.addEventListener('click', function(event) {
+event.stopPropagation();
+correctAnswer = questionsArray[i].correctAnswer;
+console.log(correctAnswer);
+    if (1 === correctAnswer) { 
+        document.getElementById("AnswerResponse").innerHTML = "Correct! Nailed it!";
+        setTimeout(function() {
+            document.getElementById("AnswerResponse").innerHTML = "";
+                },
+                1000
+            );
+        score++;
+        document.getElementById("score").innerHTML = score;
+    } else {
+        time_remaining -= 5;
+        document.getElementById("AnswerResponse").innerHTML = "Incorrect! Better luck in the next one!";
+        setTimeout(function() {
+            document.getElementById("AnswerResponse").innerHTML = "";
+                },
+                1000
+            );
+    }
+    if (i >= questionsArray.length -1) {
+    end_quiz();
+    } else {
+     i++ 
+    setQuizQuestions();
+    };
+});
+
+answerChoiceC.addEventListener('click', function(event) {
+event.stopPropagation();
+correctAnswer = questionsArray[i].correctAnswer;
+console.log(correctAnswer);
+if (2 === correctAnswer) { 
+    document.getElementById("AnswerResponse").innerHTML = "Correct! Nailed it!";
+    setTimeout(function() {
+        document.getElementById("AnswerResponse").innerHTML = "";
+            },
+            1000
+        );
+    score++;
+    document.getElementById("score").innerHTML = score;
+} else {
+    time_remaining -= 5;
+    document.getElementById("AnswerResponse").innerHTML = "Incorrect! Better luck in the next one!";
+    setTimeout(function() {
+        document.getElementById("AnswerResponse").innerHTML = "";
+            },
+            1000
+        );
 }
-// Attaches event listener to button
-resetButton.addEventListener("click", resetGame);
+if (i >= questionsArray.length -1) {
+end_quiz();
+} else {
+    i++ 
+    setQuizQuestions();
+};
+});
+
+answerChoiceD.addEventListener('click', function(event) {
+event.stopPropagation();
+correctAnswer= questionsArray[i].correctAnswer.value;
+console.log(correctAnswer);
+if (3 === correctAnswer) { 
+    document.getElementById("AnswerResponse").innerHTML = "Correct! Nailed it!";
+    setTimeout(function() {
+        document.getElementById("AnswerResponse").innerHTML = "";
+            },
+            1000
+        );
+    score++;
+    document.getElementById("score").innerHTML = score;
+} else {
+    time_remaining -= 5;
+    document.getElementById("AnswerResponse").innerHTML = "Incorrect! Better luck in the next one!";
+    setTimeout(function() {
+        document.getElementById("AnswerResponse").innerHTML = "";
+            },
+            1000
+        );
+}
+if (i >= questionsArray.length -1) {
+   end_quiz();
+} else {
+    i++ 
+    setQuizQuestions();
+};
+});
+
+    //end quiz
+    function end_quiz(){
+        document.getElementById("game_over").style.display= "block";
+        document.getElementById("quizContainer").style.display="none";
+        document.getElementById("countdownTimer").style.display= "none";
+        document.getElementById("score_keeper").style.display= "none";
+        document.getElementById("AnswerResponse").innerHTML="";
+        document.getElementById("end_score").innerHTML= score;
+        }
+
+    //submit score and initals
+        function submit_score() {
+         high_scores.push(document.getElementById("initials").value + " " + score);
+         view_high_scores();
+        }
+
+    // localStorage.setItem("score",JSON.stringify(AnswerResponse));
+    // localStorage.setItem("initials", JSON.stringify(initials));
+    
+    function view_high_scores(){
+    
+    // changing the screen output
+        document.getElementById("quizContainer").style.display="none";
+        document.getElementById("game_over").style.display= "none";
+        document.getElementById("high_scores_page").style.display="block";
+    
+        output="";
+        for(let k=0; k<high_scores.length; k++){
+             output = output + "  " + high_scores[k];
+        }
+        document.getElementById("high_scores").innerHTML= output;                
+         clear_up();
+    }
+
+    // refresh the site to the home container page
+    function go_home(){	
+            document.getElementById("high_scores_page").style.display= "none";
+            document.getElementById("homeContainer").style.display= "block";
+            clear_up();
+    }
+    
+    // clear the highscore
+    function clear_hs(){
+        high_scores = [];
+        // high_scores.splice(0, high_scores.length);
+      }
+    
+    // refresh the site 
+    function clear_up(){
+    
+    time=75;
+    time_remaining=true;
+    time_start=false;
+    i=0;
+    score=0;
+    }
